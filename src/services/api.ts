ba1e5b0b1f0690a14d8fa6255e5205df156630ee
Giovanne Leite <166829695+giovanne7@users.noreply.github.com>
2025-05-05 19:34:52 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: "https://hospedagem-tixup-back-end.onrender.com" + "/api",
   withCredentials: true,
 });
 
